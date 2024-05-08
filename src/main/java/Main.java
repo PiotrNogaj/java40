1210546a35cbebc176736a1b44295232533870d4
PiotrNogaj <161605894+PiotrNogaj@users.noreply.github.com>
2024-05-08 13:22:54 +0000

    DO DOKOŃCZENIA

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,28 +1,48 @@
-/*
-Kod bazowy programu Commit4_0: 
-• Program dodaje do prostej bazy danych (pliku db.txt) dane odnośnie Studentów.
-• Studenci dodawani są w klasie Main.
-• Wszyscy studenci są wypisywani na końcu klasy Main.
-• Klasa Service obsługuje odczyt i zapis do pliku bazy danych.
-• Klasa Student reprezentuje pojedynczego studenta (Imię, Wiek).
-*/
-
 import java.io.IOException;
+import java.util.Scanner;
 
-class Main {
-  public static void main(String[] args) {
-    try {
-      Service s = new Service();
-      s.addStudent(new Student("Krzysztof", 20));
-      s.addStudent(new Student("Janusz", 40));
+public class Main {
+    public static void main(String[] args) {
+        try {
+            Scanner scanner = new Scanner(System.in);
+            Service s = new Service();
+            s.addStudent(new Student("Krzysztof", 20));
+            s.addStudent(new Student("Janusz", 40));
 
-      var students = s.getStudents();
-      for(Student current : students) {
-        System.out.println(current.ToString());
-      }
-    } catch (IOException e) {
+            var students = s.getStudents();
+            for(Student current : students) {
+              
+            }
 
+            boolean running = true;
 
+            while (running) {
+                System.out.println("Wybierz:\n1. Dodaj studenta\n2. Wyświtl liste studentów\n3. wyjście");
+               
+                    option = scanner.nextInt();
+
+            switch(option) {
+                case 1:
+                    System.out.println("Podaj imię i nazwisko studenta:");
+                    String name = scanner.next();
+                    System.out.println("Podaj wiek studenta:");
+                    int age = scanner.nextInt();
+                    s.addStudent(new Student(name, age));
+                    break;
+                case 2:
+                    for(Student student : s.getStudents()) {
+                        System.out.println(student.toString());
+                    }
+                    break;
+                case 3:
+                    
+                default:
+                    System.out.println("Niepoprawny wybór, spróbuj ponownie.");
+                    break;
+            }
+        }
+     } catch (IOException e) {
+        e.printStackTrace();
+        }
     }
-  }
 }
